0e47817e1498d7f1b945abcd974912e3d3e624b3
MD.Ahsan Habib <68529306+BIBJAW@users.noreply.github.com>
2023-05-20 00:55:52 +0600

    Add files via upload

--- CONFIGS/i3/scripts/power.sh
+++ CONFIGS/i3/scripts/power.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
-chosen=$(printf "Log Out\nRestart\nSuspend\nPower OFF" | rofi -dmenu -i -theme-str '@import "~/.config/rofi/powermenu.rasi"')
+chosen=$(printf "Log Out\nSuspend\nRestart\nPower OFF" | rofi -dmenu -i -theme-str '@import "~/.config/rofi/powermenu.rasi"')
 
 case "$chosen" in
 	"Log Out") i3-msg exit ;;
-	"Restart") systemctl reboot ;;
 	"Suspend") i3lock -i ~/Pictures/backgrounds/coffee.png && systemctl suspend ;;
+	"Restart") systemctl reboot ;;
 	"Power OFF") systemctl poweroff ;;
 	*) exit 1 ;;
 esac
